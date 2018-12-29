9afa010ca9c27daa9789663718734c3070452d51
seokjoon <seokjoon@gmail.com>
2018-12-29 13:46:33 +0900

    styling

--- src/Styling/Css.js
+++ src/Styling/Css.js
@@ -1,8 +1,8 @@
 import React, {Component, Fragment} from 'react';
 
-import './Styling1.css'; //import style1 from './Styling1.css';
+import './Css.css'; //import style1 from './Css.css';
 
-class Styling extends Component {
+class Css extends Component {
 	render() {
 
 		const style0 = {
@@ -15,11 +15,9 @@ class Styling extends Component {
 			<Fragment>
 				<div style={style0}> Styling </div>
 				<div className="div1"> Div1 </div> { /* <div className={style1.div1}> Div1 </div> */ }
-
 			</Fragment>
-
 		);
 	}
 }
 
-export default Styling;
+export default Css;
